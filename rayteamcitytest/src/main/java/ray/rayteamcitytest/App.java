3f3b124610eba7f0cf57af3b96011fbf2aa17215
ray <rayking1988@gmail.com>
2016-12-28 10:45:25 +0800

    Merge Hello There

--- rayteamcitytest/src/main/java/ray/rayteamcitytest/App.java
+++ rayteamcitytest/src/main/java/ray/rayteamcitytest/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello New World!" );
+        System.out.println( "Hello New World1!" );
     }
 }
