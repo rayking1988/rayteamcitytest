6511748b42bee5b11e9dc7fd38d08aabfa61948b
ray <rayking1988@gmail.com>
2017-01-19 20:00:06 +0800

    Test

--- rayteamcitytest/src/main/java/ray/rayteamcitytest/App.java
+++ rayteamcitytest/src/main/java/ray/rayteamcitytest/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-    	System.out.println( "Brand new feature with issue fixed." );
+    	System.out.println( "Demo" );
     }
 }
